1476f3ccdf0c088a1fa8c3c384c6b1312780e146
Letton <lettonchannel@gmail.com>
2023-09-19 01:52:32 +0300

    3 task

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
 import "./style.scss";
+
+const target = document.querySelector("footer");
+const scrollToTopBtn = document.querySelector(".scrollTopBtn");
+
+const callback = (entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      scrollToTopBtn.classList.add("showScrollTopBtn");
+    } else {
+      scrollToTopBtn.classList.remove("showScrollTopBtn");
+    }
+  });
+};
+
+scrollToTopBtn.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+});
+
+let observer = new IntersectionObserver(callback);
+observer.observe(target);
